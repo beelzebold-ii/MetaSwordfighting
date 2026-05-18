73bdb8fa8e1c45c42c16cd5f9fd11848e474df76
beelzebold-ii <futurefast125@gmail.com>
2026-05-18 00:04:58 -0500

    we have graphics now

---
+++ assets.lua
@@ -0,0 +1,11 @@
+sprites = {
+	pIdle = love.graphics.newImage("sprites/player/IdleRight.png"),
+	pWalk = love.graphics.newImage("sprites/player/WalkRight.png"),
+	pBlock = love.graphics.newImage("sprites/player/BlockRight.png"),
+	pRaise = love.graphics.newImage("sprites/player/IdleRight.png"),
+	pSwing1 = love.graphics.newImage("sprites/player/SwingRight1.png"),
+	pSwing2 = love.graphics.newImage("sprites/player/SwingRight2.png"),
+	pHit = love.graphics.newImage("sprites/player/HitRight.png"),
+	pPull1 = love.graphics.newImage("sprites/player/PullRight1.png"),
+	pPull2 = love.graphics.newImage("sprites/player/PullRight2.png"),
+}
--- main.lua
+++ main.lua
@@ -1,1 +1,2 @@
 -- hello world
+require("assets")
